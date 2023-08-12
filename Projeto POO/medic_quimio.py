bd6f6d001b82d91025a760ac0968ac95ef28afc7
Arthur Sorrentino Ferreira <arthursorrentinoferreira@hotmail.com>
2023-08-11 21:36:51 -0300

    Alterações após comentários do professor

--- Projeto POO/medic_quimio.py
+++ Projeto POO/medic_quimio.py
@@ -1,5 +1,7 @@
+from laboratorio import Laboratorio
+
 class MedicQuimio:
-    def __init__(self, nome: str, principal_composto: str, laboratorio: str, descricao: str, necessita_receita: bool) -> None:
+    def __init__(self, nome: str, principal_composto: str, laboratorio: Laboratorio, descricao: str, necessita_receita: bool) -> None:
         self.nome = nome
         self.principal_composto = principal_composto
         self.laboratorio = laboratorio
--- Projeto POO/vendas.py
+++ Projeto POO/vendas.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 class Vendas:
-    def __init__(self, data_hora: str, produtos_vendidos: str, cliente: str, valor_total: float) -> None:
+    def __init__(self, data_hora: datetime, produtos_vendidos: str, cliente: str, valor_total: float) -> None:
         self.data_hora = data_hora
         self.produtos_vendidos = produtos_vendidos
         self.cliente = cliente
