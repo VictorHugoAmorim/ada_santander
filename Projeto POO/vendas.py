e9dcba7749804c1fb07fc5b5b626dbbcd08f615f
NW <erick_sasaki@hotmail.com>
2023-08-14 18:06:02 -0300

    Detalhado cada função

--- Projeto POO/vendas.py
+++ Projeto POO/vendas.py
@@ -2,60 +2,99 @@ from datetime import datetime
 class Vendas:
     def __init__(self) -> None:
         self.data_hora = datetime.now()
-        self.produtos_vendidos = []
+        self.produtos_vendidos = {} # AKA carrinho { nome_do_medicamento_A : {'qtd':2 , 'valor':'9.90'} }
         self.valor_total = 0.0
         self.cliente = {}
         return None
 
     def iniciar_vendas() -> None:
-        pass
         # Verifica se o cliente é cadastrado
         if not self.cliente:
-            verifica_cliente()
+            self.cliente = verifica_cliente()
 
         # Inclui medicamento que o cliente quer comprar
-        self.produtos_vendidos = formar_carrinho()
+        self.produtos_vendidos = editar_carrinho()
 
         # Verifica quanto de desconto o cliente tem
-        desconto:float = verifica_desconto(cpf) 
+        self.valor_total = calcula_total( self.produtos_vendidos )
+        desconto:float = verifica_desconto( self.cliente['idade'] , sum( self.valores_produtos) ) 
 
         # Fecha compra com valor total
-        fechamento_compra(cpf, carrinho_de_compra)
+        fechamento_compra( self.cliente[cpf], produtos_vendidos_de_compra )
+
+        # Volta ao menu principal da farmacia
+        return
 
 
     def verifica_cliente() -> dict:
         """
-        Entra com CPF ou Nome do cliente (e lista clientes que possuem esse mesmo nome digitado)
-        Se não for encontrado, alerta e orientado para cadastrar cliente
-        Se encontrado retorna o CPF em int
+        Input de CPF ou Nome do cliente (e lista clientes que possuem esse mesmo nome digitado)
+        Se não for encontrado, alerta e dá a opção de cadastrar cliente
+        
+        Se encontrado retorna os dados do cliente em formato dict
+        exemplo: {'Nome':'Erick', 'Idade':28}
         """
         pass
-        #info_cliente = input('Digite o CPF (sem pontucao) ou nome do cliente: ')
-        # Se CPF
-
-        # Se nome
     
-    def formar_carrinho() -> list:
+    def editar_carrinho(carrinho:dict) -> dict:
         """
-        entra com medicamentos no carrinho até usuário sair.
-        se for um remedio Quimioterápicos, alerta para verificar se há receita
+        Se chamar a função sem parâmetro,
+            cria um carrinho vazio
+
+        Se entrar na função já com parametros de entrada,
+            permite editar o carrinho existente
+
+        O usuario pode, em relação ao carrinho:
+            1 Visualizar
+            2 Adicionar um novo item
+                Se for um remedio Quimioterápicos, alerta para verificar se há receita
+                Se já existe no carrinho, apenas acrescentar + quantidade a ele
+            3 Alterar quantidade de um item
+                Se reduzir a zero, deleta do carrinho
+            4 Retirar item do carrinho
+        
+        Atualiza o valor self.valor_total
+            
+        saída esperada:
+        [ 'Remedio A' , 'Remedio B' , 'Remedio  C' , 'Remedio B' ] , [ 10.00 , 26.50 , 9.90 , 26.50 ]
         """
         pass
 
-    def fechamento_compra(cpf:int, lista_de_compra:list) -> None:
+    def calcula_total(carrinho: dict) -> float:
         """
-        mostra lista de medicamentos, valores unitários
-        mostra valor total (sem desconto)
-        mostra desconto
-        mostra valor a pagar
+        Entra com o carrinho atual
+        calcula o valor total com base no
+        valor unitário * quantidade de item no carrinho
+
+        retorna valor em float. # 120.90
         """
         pass
 
-"""
-Apenas para clientes cadastrados -> if cliente.CPF or cliente.Nome:
-20% de desconto para clientes idosos (acima de 65 anos) if cliente.idade > 65:
-10% de desconto nas compras acima de 150 reais ... medic_quimio.Valor_de_venda ... medic_fit.Valor_de_venda 
-Se Quimioterápicos for do tipo controlado, exige receita, emite alerta perguntando se o atendente verificou if medic_quimio.Necessita_receita
-Arquivo .json contendo um exemplo de cadastros cadastros_vendas = {}
-Classe Vendas (data_hora, produtos_vendidos, cliente, valor_total)
-"""
+    def verifica_desconto( idade:int , total:float ) -> float:
+        """
+        Entra com a idade e valor total da compra
+        Se idade do cliente acima de 65 anos
+            retorna 0.20
+        Se compra acima de 150
+            retorna 0.10
+        """
+        pass 
+
+    def fechamento_compra(cpf:int, carrinho:dict) -> None:
+        """
+        mostra a revisão do produtos_vendidos:
+            mostra lista de medicamentos, valores unitários
+            mostra valor total (sem desconto)
+            mostra desconto
+            mostra valor a pagar
+            pergunta se esta tudo certo
+        permite editar o produtos_vendidos e recalcula:
+            o valor de desconto
+            o valor total
+            retorna a revisão:
+
+        ao fechar adiciona a compra ao "banco de dados"
+        em formato dict usando como chave o datetime
+        e adiciona no arquivo .json 
+        """
+        pass
