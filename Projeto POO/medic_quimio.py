134bafd21c049e7dbf2af51f70243cf484af72ca
NWERICKSASAKI <65679096+NWERICKSASAKI@users.noreply.github.com>
2023-08-14 16:46:20 -0300

    Update medic_quimio.py
    Adicionado atributo de valor para ser puxado ao formar a venda.

--- Projeto POO/medic_quimio.py
+++ Projeto POO/medic_quimio.py
@@ -1,9 +1,10 @@
 from laboratorio import Laboratorio
 
 class MedicQuimio:
-    def __init__(self, nome: str, principal_composto: str, laboratorio: Laboratorio, descricao: str, necessita_receita: bool) -> None:
+    def __init__(self, nome: str, principal_composto: str, laboratorio: Laboratorio, descricao: str, necessita_receita: bool, valor: float) -> None:
         self.nome = nome
         self.principal_composto = principal_composto
         self.laboratorio = laboratorio
         self.descricao = descricao
         self.necessita_receita = necessita_receita
+        self.valor = valor
