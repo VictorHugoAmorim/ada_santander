1e9d2a95b3cb9ae4ba3e3ad9ec6b71726facae5d
Victor Hugo <victor.hugo.amorim.7@gmail.com>
2023-08-16 01:07:04 -0300

    direectory changes and fineshed register customer class

--- Projeto POO/cadastra_cliente.py
+++ Projeto POO/cadastra_cliente.py
@@ -1,4 +1,9 @@
-
+#Variável global que inicializa o dicionário de cadastros
+cadastros_clientes = {
+    'identificador': [],
+    'nome': [],
+    'data_nascimento': []
+}
 class Cadastro:
 
     def __init__(self, identificador: str, nome: str, data_nascimento: str) -> None:
@@ -6,8 +11,27 @@ class Cadastro:
 
     def coleta_dados(self):
         """
-        Nesta função será coletado os dados enqunto se verifica as entrada possíveis entradas incorretas do usuário.
-        O código mantem redundancia das coletas até que o usuário forneça uma entrada válida
+        Nesta função será coletado a partir da exigencias de formatação específicas de cada método
+        """
+        print('#'*30,'CADASTRO DE CLIENTES','#'*30)
+        identificador = self.valida_identificador()
+        nome = self.valida_nome()
+        data_nascimento = self.valida_data()
+        return identificador, nome, data_nascimento
+
+    def armazena_dados(self):
+        """
+        Nesta função serão armazenados os dados coletados da função 'coleta_dados'.
+        Logo após, serão adicionadas a uma estrutura de dicionário.
+        """
+        cadastros_clientes['identificador'].append(self.identificador)
+        cadastros_clientes['nome'].append(self.nome)
+        cadastros_clientes['data_nascimento'].append(self.data_nascimento)
+        return cadastros_clientes
+    
+    def valida_identificador(self):
+        """
+        Validador para identificação, insiste que o usuário digite a quantidade e formato correto
         """
         while True:
             identificador = input('Digite o CPF: [Apenas números - 11 dígitos]\n').strip()
@@ -15,10 +39,16 @@ class Cadastro:
                break
             else:
                 print('Formato de CPF inválido')
+        return identificador
+    
+    def valida_nome(self):
         while True:
             nome = input('Digite o nome:\n').strip().upper()
             if len(nome) > 0 and nome.isprintable() and nome.isdigit() == False:
                 break
+        return nome
+    
+    def valida_data(self):
         while True:
             print('Por favor, forneça sua data de nascimento:\n')
             dia = input('Digite o dia do nascimento: [dd]\n').strip()
@@ -32,29 +62,51 @@ class Cadastro:
                 break
             else:
                 print('Formato de data inválido\n')
-        return identificador, nome, data_nascimento
+        return data_nascimento
 
-    def armazena_dados(self):
-        """
-        Nesta função serão armazenados os dados coletados da função 'coleta_dados'.
-        Logo após, serão adicionadas a uma estrutura de dicionário.
-        """
-        cadastros_clientes = {
-            'identificador': [],
-            'nome': [],
-            'data_nascimento': []
-        }
-        cadastros_clientes['identificador'].append(self.identificador)
-        cadastros_clientes['nome'].append(self.nome)
-        cadastros_clientes['data_nascimento'].append(self.data_nascimento)
-        return cadastros_clientes
-    
+    def altera_cliente(self, cadastros_clientes):
+        cadastros_clientes = cadastros_clientes
+        print('#'*30,'ALTERAÇÕES DE CLIENTES','#'*30)
+        while True:
+            cliente_escolhido = input('Digite o CPF do cliente [Apenas números - 11 dígitos] ou enter para sair:\n').strip()
+            if cliente_escolhido in cadastros_clientes['identificador']:
+                idx = cadastros_clientes['identificador'].index(cliente_escolhido)
+                print(f'cliente {cadastros_clientes["nome"][idx]} encontrado')
+                escolha_alteracao = input('Escolha qual campo deseja alterar:\n1-CPF:\n2-Nome:\n3-Data de Nascimento:\n4-Sair:\n').strip()
+                if escolha_alteracao == '4':
+                    break
+                elif escolha_alteracao == '1':
+                    novo_cpf = self.valida_identificador()
+                    cadastros_clientes['identificador'][idx] = novo_cpf
+                    print(f'Cliente {cadastros_clientes["nome"][idx]} agora possui o CPF: {cadastros_clientes["identificador"][idx]}')
+                elif escolha_alteracao == '2':
+                    novo_nome = self.valida_nome()
+                    cadastros_clientes['nome'][idx] = novo_nome
+                    print(f'Cliente {cadastros_clientes["nome"][idx]} teve seu nome atualizado')
+                elif escolha_alteracao == '3':
+                    nova_data = self.valida_data()
+                    cadastros_clientes['data_nascimento'][idx] = nova_data
+                    print(f'Cliente {cadastros_clientes["nome"][idx]} agora possui a data de nascimento: {cadastros_clientes["data_nascimento"][idx]}')
+            else:
+                if len(cadastros_clientes) > 0:
+                    print('CPF incorreto ou não cadastrado')
+                break
+
+    def exibe_clientes(self, cadastros_clientes):
+        print('#'*30,'EXIBIÇÃO DE CLIENTES','#'*30)
+        cadastros_clientes = cadastros_clientes
+        num_clientes = len(cadastros_clientes['identificador'])
+        for i in range(num_clientes):
+            print(f"CPF: {cadastros_clientes['identificador'][i]}")
+            print(f"nome: {cadastros_clientes['nome'][i]}")
+            print(f"data_nascimento: {cadastros_clientes['data_nascimento'][i]}\n")
+
     @staticmethod
     def exec():
         cadastro = Cadastro('','','')
         cadastros_clientes = cadastro.armazena_dados()
-        return cadastros_clientes
+        cadastro.armazena_dados()
+        cadastro.altera_cliente(cadastros_clientes)
 
 if __name__ == "__main__":
     cadastros = Cadastro.exec()
-    print(cadastros)
--- Projeto POO/main.py
+++ Projeto POO/main.py
@@ -1,4 +1,5 @@
 from cadastra_cliente import *
+from cadastra_cliente import Cadastro
 
 
 def sys():
@@ -6,16 +7,27 @@ def sys():
     while True:
         print('1- Cadastrar cliente\n2- Alterar cadastro de cliente\n3- Consultar clientes cadastrados',sep='')
         print('4- Cadastrar medicamento\n5- Alterar cadastro de medicamento\n6- Consultar medicamentos cadastrados\n0- Sair')
-        user_choice = input('Por favor, escolha o número da ação desejada: ')
+        user_choice = input('Por favor, escolha o número da ação desejada:\n ')
         if user_choice == '0':
             break
         elif user_choice in ['1','2','3','4','5']:    
             #Case 1
             if user_choice == '1':
-                Cadastro.exec()
+                cadastro = Cadastro('','','')
+                cadastros_clientes = cadastro.armazena_dados()
             
+            elif user_choice == '2':
+                try:
+                    cadastro.altera_cliente(cadastros_clientes)
+                except:
+                    print('Impossivel, nenhum cadastro encontrado!\n')
+            elif user_choice == '3':
+                try:
+                    cadastro.exibe_clientes(cadastros_clientes)
+                except:
+                    print('Impossivel, nenhum cadastro encontrado!\n')
             #Continuar a partir daqui
-        break
+        
 
 if __name__ == '__main__':
     sys()
