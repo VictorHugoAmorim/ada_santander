6b6b822234426cd92bace35bcb7b44aed20fa8d2
NWErickSasaki <erick_sasaki@hotmail.com>
2023-08-17 11:55:47 -0300

    Separado o carrinho em uma classe exclusiva

--- Projeto POO/vendas.py
+++ Projeto POO/vendas.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from functools import reduce
 from cadastra_cliente import Cadastro
+from medicamentos import Medicamentos
 
 # por NWErickSasaki
 # TODO adicionar get ou property em dados sensiveis
@@ -52,55 +53,6 @@ class Vendas:
                     return None
         return cpf
 
-    def editar_carrinho(self) -> dict:
-        opcao = ""
-        while not opcao == "0":
-            print('\n#'*30,'CARRINHO DE COMPRAS','#'*30)
-            opcao = input("""
-                    Digite o número correspondente para:
-                        
-                    1 Visualizar carrinho
-                    2 Adicionar um novo item
-                    3 Alterar quantidade de um item
-                    4 Retirar item do carrinho
-                    0 Finalizar venda
-                    """)
-            match opcao:
-                case '1':
-                    pass
-                    # visualiza carrinho
-                    """
-                    01 x Remedio A - R$ 120.00
-                    03 x Remedio B - R$ 9.90
-                    ----------------------------
-                    Total = R$ 149.70
-                    """
-                case '2':
-                    # Se for um remedio Quimioterápicos, alerta para verificar se há receita
-                    # Se já existe no carrinho, apenas acrescentar + quantidade a ele
-                    pass
-                case '3':
-                    pass
-                    # Se reduzir a zero, deleta do carrinho
-                case '4':
-                    pass
-                case '0':
-                    pass
-                case _:
-                    print(f"A opcao '{opcao}' é invalida!")
-        """
-        Atualiza o valor self.valor_total
-            
-        saída esperada:
-        {'Remedio A': {'Preço':120.00, 'Qtd':1} ,
-         'Remedio B': {'Preço':9.90, 'Qtd':3}  }
-        """
-        pass
-
-    def calcula_total(self) -> float:
-        soma = lambda element , inicio : element + inicio
-        return reduce(soma, (self.carrinho[itens]['Preço']*self.carrinho[itens]['Qtd'] for itens in self.carrinho) , 0)
-
     def verifica_desconto( idade:int , total_da_compra:float ) -> float:
         return 0.2 if idade > 65 else 0.1 if total_da_compra > 150 else 0
 
@@ -123,10 +75,84 @@ class Vendas:
         """
         pass
 
+
+class carrinho_de_vendas:
+
+    def __init__(self):
+        self.carrinho = {}
+        _valor_total = 0
+
+    def __repr__(self) -> str: # TODO
+        print("""
+        01 x Remedio A - R$ 120.00
+        03 x Remedio B - R$ 9.90
+        ----------------------------
+        Total = R$ 149.70
+        """)
+
+
+    def editar_carrinho(self) -> dict:
+        opcao = ""
+        while not opcao == "0":
+            print('\n#'*30,'CARRINHO DE COMPRAS','#'*30)
+            opcao = input("""
+                    Digite o número correspondente para:
+                        
+                    1 Visualizar carrinho
+                    2 Adicionar um novo item
+                    3 Alterar quantidade de um item
+                    4 Retirar item do carrinho
+                    0 Finalizar venda
+                    """)
+            match opcao:
+                case '1':
+                    print(self)
+
+                case '2':
+                    #remedio_a_add = Medicamentos
+                    #localiza_remedio_por_input()
+                    #verifica_se_precisa_de_receita()
+                    pass
+
+                case '3':
+                    # Se reduzir a zero, deleta do carrinho
+                    pass
+                
+                case '4':
+                    pass
+
+                case '0':
+                    pass
+
+                case _:
+                    print(f"A opcao '{opcao}' é invalida!")
+        """
+        Atualiza o valor self.valor_total
+            
+        saída esperada:
+        {'Remedio A': {'Preço':120.00, 'Qtd':1} ,
+         'Remedio B': {'Preço':9.90, 'Qtd':3}  }
+        """
+        pass
+
+    @property
+    def valor_total(self) -> float:
+        soma = lambda element , inicio : element + inicio
+        self._valor_total = reduce(soma, (self.carrinho[itens]['Preço']*self.carrinho[itens]['Qtd'] for itens in self.carrinho) , 0)
+        return self._valor_total
+
+    def localiza_remedio_por_input(self):
+        # Input
+        # Se for um remedio Quimioterápicos, alerta para verificar se há receita
+        # Se já existe no carrinho, apenas acrescentar + quantidade a ele
+        pass
+
+
+ # Teste ----------------------------------------------------------
+
 teste = True
 if(teste):
-    nova_venda = Vendas()
-    nova_venda.carrinho = {'Remedio A': {'Preço':120.00, 'Qtd':1} ,
-                        'Remedio B': {'Preço':9.90, 'Qtd':3}  }
-
-    print(nova_venda.calcula_total())
+    novo_carrinho = carrinho_de_vendas()
+    novo_carrinho.carrinho = {'Remedio A': {'Preço':120.00, 'Qtd':1} ,
+                             'Remedio B': {'Preço':9.90, 'Qtd':3}  }
+    print(novo_carrinho.valor_total)
