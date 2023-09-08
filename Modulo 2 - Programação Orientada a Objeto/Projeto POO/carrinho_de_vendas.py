67a087ec712811fc1538d241483c8b62d302752b
Victor Hugo <victor.hugo.amorim.7@gmail.com>
2023-09-08 11:23:51 -0300

    update directories

---
+++ Modulo 2 - Programação Orientada a Objeto/Projeto POO/carrinho_de_vendas.py
@@ -0,0 +1,199 @@
+from medicamentos import Medicamentos
+from medic_fit import MedicFit
+from medic_quimio import MedicQuimio
+from functools import reduce
+
+# by NWErickSasaki
+
+class Carrinho_de_vendas:
+
+    def __init__(self, carrinho:list=[[],[]]):
+        self.carrinho = carrinho # [ [obj_Med_A , obj_Med_B , ... ] , [ 1 , 3 , ... ] ]
+        self._valor_total = self.valor_total
+
+    def __repr__(self) -> str: # TODO
+        txt =  """
+        \t-------------------------- COMPRA --------------------------\n
+        \tID\tMEDICAMENTO\tQTD\tPREÇO\t\tSUB-TOTAL\n\n"""
+        for idx,itens in enumerate(self.carrinho[0], start=1):
+            txt += f"\t\t{idx}\t{itens.nome.upper()}\t{str(self.carrinho[1][idx-1])}\tR$ {itens.valor:.2f}\tR$ {itens.valor*int(self.carrinho[1][idx-1]):.2f}   \n"
+        txt += f"\n\t\tTOTAL \t\tR$ {self.valor_total:.2f}"
+        return txt
+
+    @property
+    def valor_total(self) -> float:
+        lista_em_duplas = [ ] # [ [rem,1] , [rem_A,3] ... ]
+        for i in range (len(self.carrinho[0])):
+            lista_em_duplas.append([self.carrinho[0][i],self.carrinho[1][i]])
+        soma = lambda element , inicio : element + inicio
+        self._valor_total = reduce(soma, ( itens[0].valor * int(itens[1]) for itens in lista_em_duplas) , 0)
+        return self._valor_total
+
+    def editar_carrinho(self) -> dict:
+        opcao = ""
+        while not opcao == "0":
+            print('\n','#'*30,'CARRINHO DE COMPRAS','#'*30)
+            opcao = input("""
+                    1 Visualizar carrinho
+                    2 Adicionar um novo item
+                    3 Alterar quantidade de um item
+                    4 Retirar item do carrinho
+                    0 Finalizar venda
+                    
+                    Digite um numero: """)
+            
+            match opcao:
+                case '1':
+                    print(self)
+
+                case '2':
+                    remedio_selecionado=self.localiza_remedio_por_input()
+                    if self.medicamento_ja_no_carrinho(remedio_selecionado):
+                        self.altera_no_carrinho_a_quantidade_do(remedio_selecionado)
+                        continue
+                    elif self.verifica_se_precisa_de_receita(remedio_selecionado):
+                        if not self.cliente_tem_receita_para(remedio_selecionado):
+                            print(f'Nao adicionado ao carrinho: {remedio_selecionado.nome}')
+                            continue
+                    quantidade = self.pede_input_de_quantidade_desejada_de(remedio_selecionado)
+                    self.adiciona_no_carrinho(remedio_selecionado, quantidade)
+        
+                case '3':
+                    remedio_selecionado = self.localiza_remedio_por_input(onde=self.carrinho[0])
+                    self.altera_no_carrinho_a_quantidade_do(remedio_selecionado)
+
+                case '4':
+                    remedio_selecionado = self.localiza_remedio_por_input(onde=self.carrinho[0])
+                    self.retirar_do_carrinho(remedio_selecionado) 
+
+                case '0':
+                    return self
+
+                case _:
+                    print(f"A opcao '{opcao}' é invalida!")
+
+    def altera_no_carrinho_a_quantidade_do(self, remedio_selecionado:Medicamentos, nova_quantidade:int=0) -> None:
+        nova_quantidade = (self.pede_input_de_quantidade_desejada_de(remedio_selecionado) if nova_quantidade==0 else nova_quantidade)
+        if nova_quantidade <= 0:
+            self.retirar_do_carrinho(remedio_selecionado)
+        elif remedio_selecionado in self.carrinho[0]:
+            idx = self.carrinho[0].index(remedio_selecionado)
+            self.carrinho[1][idx] = nova_quantidade
+            print(f'Alterado para: {nova_quantidade} x {remedio_selecionado.nome}')
+        else:
+            self.adiciona_no_carrinho(remedio_selecionado, nova_quantidade)
+        return None
+
+
+    def cliente_tem_receita_para(self, remedio_selecionado:Medicamentos) -> bool:
+        num = input(f"""
+        O Cliente tem receita para
+        "{remedio_selecionado.nome}"?
+
+        1 - Sim
+        2 - Não
+        * - Voltar
+        
+        Digite o numero correspondente: """).strip()
+        return (True if num == "1" else False)
+
+    def verifica_se_precisa_de_receita(self, remedio:Medicamentos)-> bool:
+        if isinstance(remedio, MedicQuimio):
+            return remedio.necessita_receita
+        return False
+
+    def medicamento_ja_no_carrinho(self, remedio_selecionado:Medicamentos) -> bool:
+        if remedio_selecionado in self.carrinho[0]:
+            return True
+        elif not remedio_selecionado in self.carrinho[0]:
+            return False
+        else:
+            raise ValueError
+
+    def pede_input_de_quantidade_desejada_de(self, remedio_selecionado:Medicamentos) -> int:
+        num = -1
+        while num != 0:
+            num = input(f'Digite a quantidade deseja de "{remedio_selecionado.nome}" que gostaria de ter no carrinho: ').strip()
+            if num.isdigit():
+                num = int(num)
+                return num
+
+    def adiciona_no_carrinho(self, remedio_selecionado:Medicamentos, quantidade:int=1) -> None:
+        if quantidade:
+            self.carrinho[0].append(remedio_selecionado)
+            self.carrinho[1].append(quantidade)
+            print(f'Adicionado: {quantidade} x "{remedio_selecionado.nome}"')
+            return None
+        print(f"Nao foi adicionado ao carrinho: {remedio_selecionado}")
+
+    def retirar_do_carrinho(self, remedio_selecionado:Medicamentos) -> Medicamentos:
+        if remedio_selecionado in self.carrinho[0]:
+            idx = self.carrinho[0].index(remedio_selecionado)
+            self.carrinho[0].pop(idx)
+            self.carrinho[1].pop(idx)
+            print(f'Retirado do carrinho: {remedio_selecionado.nome}')
+        return remedio_selecionado
+
+    def localiza_remedio_por_input( self , palavra_do_remedio:str="" , onde:list=Medicamentos.lista_medicamentos ) -> Medicamentos:
+        if not palavra_do_remedio:
+            palavra_do_remedio =  input('Digite qual remedio está procurando:')
+        palavra_do_remedio = palavra_do_remedio.lower()
+
+        lista_remedios_por_nome = [ remedio for remedio in onde if palavra_do_remedio in remedio.nome.lower()]
+        lista_remedios_por_principal_composto = [ remedio for remedio in onde if palavra_do_remedio in remedio.principal_composto.lower()]
+        lista_remedios_por_descricao = [ remedio for remedio in onde if palavra_do_remedio in remedio.descricao.lower()]
+        lista_remedios_por_laboratorio = [ remedio for remedio in onde if palavra_do_remedio in remedio.laboratorio.nome.lower()]
+        lista_todos = lista_remedios_por_nome + lista_remedios_por_principal_composto + lista_remedios_por_laboratorio + lista_remedios_por_descricao
+        
+        dict_remedios={}
+        for e in lista_todos:
+            dict_remedios[e] = e
+        lista_de_remedio = list(dict_remedios)
+
+        if not lista_de_remedio:
+            print('Medicamento nao encontrado!! :(')
+            self.localiza_remedio_por_input("", onde)
+        elif len(lista_de_remedio)>1:
+            return self.escolha_um_remedio_na(lista_de_remedio)
+        else:
+            return lista_de_remedio[0]  ### TODO da pra otimizar porem quando a lista só tem index 0 quebra
+
+    def escolha_um_remedio_na(self, lista_de_remedio: list) -> Medicamentos:
+        print('Digite o numero correspondente do remedio que gostaria de adicionar ou 0 para sair:\n')
+        for idx,itens in enumerate(lista_de_remedio, start = 1):
+            print(idx, itens,'\n')
+        num = -1
+        while not num in range(1,len(lista_de_remedio)+1):
+            num = input('Digite um numero: ').strip()
+            num = ( int(num) if num.isdigit() else -1 )
+        return False if num==0 else lista_de_remedio[num-1]
+
+    def remedio_mais_vendido_do_carrinho_e_quanto(carrinho) -> (Medicamentos, int):
+        quantidade = max(carrinho[1])
+        idx = carrinho[1].index(str(quantidade))
+        remedio_mais_vendido = carrinho[0][idx]
+        return (remedio_mais_vendido, quantidade)
+
+    def filtrar_carrinho_por_classe(carrinho, classe:classmethod):
+        carrinho_filtrado = [ [ ] , [ ] ]
+        lista_med = []
+        lista_qtd = []
+        for i,med in enumerate(carrinho[0]):
+            if isinstance(med, classe):
+                lista_med.append(carrinho[0][i])
+                lista_qtd.append(carrinho[1][i])
+        carrinho_filtrado[0]=lista_med
+        carrinho_filtrado[1]=lista_qtd
+        return carrinho_filtrado
+    
+# Teste ----------------------------------------------------------
+
+TESTE = 0
+if TESTE:
+    print('\n\n VOCE ESTA RODANDO UM TESTE \n\n')
+    from lendocsv import lendo_csv
+    lendo_csv()
+    nc = Carrinho_de_vendas()
+    nc.editar_carrinho()
+
+    # erro ao puxar carrinho [,[0,2]]
---
+++ Modulo 2 - Programação Orientada a Objeto/Projeto POO/vendas.py
@@ -0,0 +1,272 @@
+from datetime import datetime
+from cadastra_cliente import *
+from cadastra_cliente import Cadastro
+from medicamentos import Medicamentos
+from carrinho_de_vendas import Carrinho_de_vendas
+from datetime import date
+from medic_fit import MedicFit
+from medic_quimio import MedicQuimio
+import csv
+from functools import reduce
+
+IDADE_IDOSO = 65
+VALOR_COMPRA_GRANDE = 150
+DESCONTO_PORCENTAGEM_IDOSO = 0.2
+DESCONTO_PORCENTAGEM_COMPRA_GRANDE = 0.1
+
+# by NWErickSasaki
+# TODO sigilo no valor do carrinho, cliente na compra, CPF do cliente
+class Vendas:
+
+    cadastro_vendas=[] # [ obj_vendas1, obj_vendas2 ]
+
+    def __init__(self, cliente:Cadastro=Cadastro(), data_hora:datetime=datetime.now(), produtos_vendidos:Carrinho_de_vendas=Carrinho_de_vendas()) -> None:
+        self.cliente = cliente
+        self.data_hora = data_hora
+        self.produtos_vendidos = produtos_vendidos # [ [obj_Med_A , obj_Med_B , ... ] , [ 1 , 3 , ... ] ]
+        self.atualiza_valores()
+        return None
+
+    def __repr__(self):
+        return(f"""
+        \t############################################################  
+
+        \tCLIENTE\t{self.cliente.nome.upper()}
+        \tCPF\t{self.cliente.identificador}
+        {self.produtos_vendidos}
+        \tDESCONTO\tR$ {self._valor_de_desconto:.2f}\t({self._porcentagem_de_desconto*100:.2f}%)
+        \tTOTAL A PAGAR \tR$ {(self._valor_total_com_desconto):.2f}
+
+        \t############################################################""")
+
+    def atualiza_valores(self) -> None:
+        self._valor_total_sem_desconto = self.produtos_vendidos.valor_total
+        self._porcentagem_de_desconto = Vendas.verifica_porcentagem_de_desconto( self.cliente.idade , self._valor_total_sem_desconto )
+        self._valor_de_desconto = self.porcentagem_de_desconto * self._valor_total_sem_desconto
+        self._valor_total_com_desconto = self._valor_total_sem_desconto - self.valor_de_desconto
+        return None
+
+    @property
+    def valor_total_sem_desconto(self):
+        self._valor_total_sem_desconto = self.produtos_vendidos.valor_total
+        return self._valor_total_sem_desconto
+
+    @property
+    def porcentagem_de_desconto(self):
+        return self._porcentagem_de_desconto
+    
+    @porcentagem_de_desconto.setter
+    def porcentagem_de_desconto(self, porcentagem):
+        if (porcentagem == DESCONTO_PORCENTAGEM_IDOSO and self.cliente.idade > IDADE_IDOSO) or (porcentagem == DESCONTO_PORCENTAGEM_COMPRA_GRANDE and self._valor_total_sem_desconto > VALOR_COMPRA_GRANDE):
+            self._porcentagem_de_desconto = porcentagem
+        else:
+            print("ALERTA! Porcentagem de desconto nao autorizado!")
+
+    @property
+    def valor_de_desconto(self):
+        return self._valor_de_desconto
+
+    @valor_de_desconto.setter
+    def valor_de_desconto(self, novo_valor):
+        if (novo_valor*self.porcentagem_de_desconto == novo_valor*DESCONTO_PORCENTAGEM_IDOSO and self.cliente.idade > IDADE_IDOSO) or (novo_valor*self.porcentagem_de_desconto == novo_valor*DESCONTO_PORCENTAGEM_COMPRA_GRANDE and self._valor_total_sem_desconto > VALOR_COMPRA_GRANDE):
+            self._valor_de_desconto = novo_valor
+        else:
+            print("ALERTA! Valor de desconto nao autorizado!")
+
+    @property
+    def valor_total_com_desconto(self):
+        self._valor_total_com_desconto = self._valor_total_sem_desconto - self.valor_de_desconto
+        return self._valor_total_com_desconto
+
+    def iniciar_vendas(self) -> None: # TODO
+        print("Para realizar uma venda, por favor, informe o cliente.")
+        self.cliente = self.encontrar_cliente_por_cpf()
+        print(f"\nCliente localizado. \nBem vindo {self.cliente.nome} \nnIniciando venda.")
+        self.produtos_vendidos  = Carrinho_de_vendas().editar_carrinho()       
+        self.fechamento_compra()
+        print(self)
+        Vendas.salva_banco(self)
+        return None
+
+    def encontrar_cliente_por_cpf(self) -> Cadastro: # TODO
+        cpf = Cadastro.valida_identificador()
+        while cpf not in cadastros_clientes['identificador']:
+            opcao = input("""
+                  CPF não encontrado no sistema.
+                  Digite o número correspondente para: 
+                  1 - Cadastrar cliente
+                  * - Buscar outro CPF
+                  """)
+            match opcao:
+                case '1':
+                    novo_cliente = Cadastro()
+                    a,b,c,d = novo_cliente.coleta_dados()
+                    novo_cliente = Cadastro(a,b,c,d)
+                    novo_cliente.armazena_dados()
+                    cpf = novo_cliente.identificador
+                case _:
+                    cpf = Cadastro.valida_identificador()
+        return Vendas.pega_cliente_com_cpf(cpf)
+        #return Cadastro('12345678901','Erick Teste', date(2000,1,1)) # para usar para teste enquanto a função acima for implementada
+
+    def verifica_porcentagem_de_desconto( idade:int , total_da_compra:float ) -> float:
+        return DESCONTO_PORCENTAGEM_IDOSO if idade > IDADE_IDOSO else DESCONTO_PORCENTAGEM_COMPRA_GRANDE if total_da_compra > VALOR_COMPRA_GRANDE else 0
+
+    def fechamento_compra(self) -> None: # TODO
+        self.atualiza_valores()
+        Vendas.cadastro_vendas.append(self)
+        return None
+
+    def salva_banco(self) -> None:
+        str_de_produtos = ','.join([e.nome for e in self.produtos_vendidos.carrinho[0]])
+        str_de_precos = ','.join([str(e) for e in self.produtos_vendidos.carrinho[1]])
+        try:
+            PATH = 'Projeto POO/csv/'
+            f = open(f'{PATH}vendas.csv','a', encoding='utf-8')
+        except:
+            PATH = '.\csv\''
+            f = open(f'{PATH}vendas.csv','a', encoding='utf-8')
+        f.write(f'\n{self.cliente.identificador};{self.data_hora};{str_de_produtos};{str_de_precos}')
+        f.close()
+        return None
+
+    def carrega_banco() -> None:
+        try:
+            PATH = 'Projeto POO/csv/'
+            f = open(f'{PATH}vendas.csv','r', encoding='utf-8')
+        except:
+            PATH = '.\csv\''
+            f = open(f'{PATH}vendas.csv','r', encoding='utf-8')
+
+        conteudo = csv.DictReader(f, delimiter=";")
+        for linha in conteudo:
+            cpf = linha['IDENTIFICADOR']
+            cliente = Vendas.pega_cliente_com_cpf(cpf) # TODO
+            data_hora = datetime.strptime(linha['DATA_HORA'],"%Y-%m-%d %H:%M:%S.%f").date()
+            lista_produtos = linha['PRODUTOS'].split(',')
+            obj_produtos = [ Vendas.nome_medicamento_pra_obj(nome_produtos) for nome_produtos in lista_produtos ]
+            quantidade = linha['QUANTIDADE'].split(',')
+            novo_carrinho = Carrinho_de_vendas([ obj_produtos , quantidade ]) 
+            nova_venda = Vendas(cliente, data_hora, novo_carrinho)
+            Vendas.cadastro_vendas.append(nova_venda)
+        f.close()
+        return None
+    
+    def pega_cliente_com_cpf(cpf) -> Cadastro:
+        idx = cadastros_clientes['identificador'].index(cpf)
+        identificador = cadastros_clientes['identificador'][idx]
+        nome = cadastros_clientes['nome'][idx]
+        data_nascimento = cadastros_clientes['data_nascimento'][idx]
+        idade = cadastros_clientes['idade'][idx]
+        obj_cliente = Cadastro(identificador,nome,data_nascimento,idade)
+        return obj_cliente
+
+    def nome_medicamento_pra_obj(nome):
+        for idx, med in enumerate(Medicamentos.lista_medicamentos):
+            if med.nome == nome:       
+                return Medicamentos.lista_medicamentos[idx]
+        raise
+
+    def relatorio_de_estatistica_de_venda():
+        lista_de_todas_as_vendas_de_hoje = [vendas for vendas in Vendas.cadastro_vendas if vendas.data_hora == datetime.today().date()]
+        carrinho_hoje = Vendas.unifica_lista_de_compras_em_uma_unica_lista(lista_de_todas_as_vendas_de_hoje)
+
+        if not carrinho_hoje:
+            print('Nao teve vendas hoje')
+            return
+
+        med,qtd_med = Carrinho_de_vendas.remedio_mais_vendido_do_carrinho_e_quanto(carrinho_hoje)
+        qtd_pessoas, qtd_vendas = Vendas.quantidade_de_pessoas_e_vendas_realizadas(lista_de_todas_as_vendas_de_hoje)
+        
+        carrinho_quimio = Carrinho_de_vendas.filtrar_carrinho_por_classe(carrinho_hoje, MedicQuimio)
+        quantos_med_quimio, quantos_tipos_quimio, total_quimio = Vendas.quantos_medicamentos_e_quantos_tipos_e_valor_no_carrinho(carrinho_quimio)
+
+        carrinho_fit = Carrinho_de_vendas.filtrar_carrinho_por_classe(carrinho_hoje, MedicFit)
+        quantos_med_fit, quantos_tipos_fit, total_fit = Vendas.quantos_medicamentos_e_quantos_tipos_e_valor_no_carrinho(carrinho_fit)
+
+        print(f"""
+              
+        ---------------- ESTATISTICA DE VENDA ----------------
+        
+        O medicamento mais vendido foi: {med.nome}
+        Unidades vendidas: {qtd_med}
+        Valor arrecadado: R$ {(int(qtd_med) * med.valor):.2f}
+
+        Houve um numero total de vendas de: {qtd_vendas}
+        Com um numero de clientes diferentes de: {qtd_pessoas}
+
+        MEDICAMENTO QUIMIOTERÁPICO
+        Total de medicamentos vendidos: {quantos_med_quimio}
+        Total de tipos diferentes de medicamentos vendidos: {quantos_tipos_quimio}
+        Total arrecadado: R$ {total_quimio:.2f}
+
+        MEDICAMENTO FITOTERÁPICO
+        Total de medicamentos vendidos: {quantos_med_fit}
+        Total de tipos diferentes de medicamentos vendidos: {quantos_tipos_fit}
+        Total arrecadado: R$ {total_fit:.2f}
+
+        """)  
+
+    def quantos_medicamentos_e_quantos_tipos_e_valor_no_carrinho(carrinho) -> (int,int,float):
+        quantos_medicamentos = sum([int(element) for element in carrinho[1]])
+        quantos_tipos = len(carrinho[0])
+        lista_valores_totais = [float(carrinho[0][i].valor) * int(carrinho[1][i]) for i in range(quantos_tipos)]
+        total_carrinho = sum(lista_valores_totais)
+        return quantos_medicamentos , quantos_tipos , total_carrinho  
+            
+    def quantidade_de_pessoas_e_vendas_realizadas(lista_de_vendas:list) -> (int,int):
+        return ( len(set(lista_de_vendas)) , len(lista_de_vendas) )
+        
+    def unifica_lista_de_compras_em_uma_unica_lista(lista_de_vendas:list) -> list:
+        lista_de_med = reduce( lambda array, ini: ini + array,  [ venda.produtos_vendidos.carrinho[0] for venda in lista_de_vendas ] , [])
+        lista_de_qtd = reduce( lambda array, ini: ini + array,  [ venda.produtos_vendidos.carrinho[1] for venda in lista_de_vendas ] , [])
+        lista_return = [ [] , [] ]
+        for i,med in enumerate(lista_de_med):
+            if not med in lista_return[0]:
+                lista_return[0].append(med)
+                lista_return[1].append(lista_de_qtd[i])
+            else:
+                idx = lista_return[0].index(med)
+                lista_return[1][idx]=str (int(lista_de_qtd[i])+int(lista_return[1][idx]))
+        return lista_return
+    
+    def input_qual_classe_de_remedio() -> Medicamentos:
+        opcao = 0
+        while not opcao in range(1 , 3+1):
+            input("""
+            Qual Classe de remedio gostaria de ver os relatorios?
+            1 - Todos
+            2 - Quimioterápicos
+            3 - Fitoterápicos
+            
+            Digite o numero correspondente: """).strip()
+            if opcao.isdigit():
+                opcao = int(opcao)
+        match opcao:
+            case 1: return MedicQuimio
+            case 2: return MedicFit
+            case _: return False
+
+
+
+
+# Teste ----------------------------------------------------------
+
+TESTE = 0
+if TESTE:
+    print('\n\n VOCE ESTA RODANDO UM TESTE \n\n')
+    import lendocsv
+    lendocsv.lendo_csv()
+
+    Vendas.carrega_banco()
+    print(Vendas.cadastro_vendas)
+
+    eu = Cadastro('12345678901','Erick Teste', date(2000,1,1))
+    eu.armazena_dados()
+
+    #print(eu.nome)
+    #print(eu.identificador)
+    #print(eu.data_nascimento)
+
+    nv = Vendas()
+    nv.iniciar_vendas()
