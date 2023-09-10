eb81e3d6f2cfe9d1148d8c8f0f50d7521fab5071
Victor Hugo <64938014+VictorHugoAmorim@users.noreply.github.com>
2023-09-10 14:49:49 -0300

    Adicionando padroes de dados e arquivo de requerimentos (#20)

--- Modulo 3 - Tecnicas de Programacao I (PY)/Projeto Limpeza de dados/src/projeto.py
+++ Modulo 3 - Tecnicas de Programacao I (PY)/Projeto Limpeza de dados/src/projeto.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime 
 import re
+from unidecode import unidecode
 
 #Diretórios
 DIR_ESCOLAS = r'..\arquivos csv\escolas.csv'
@@ -12,17 +13,36 @@ DIR_SUB_PREFEITURAS = r'..\arquivos csv\subprefeituras.csv'
 #Importação com tratamento de colunas
 def import_csv(arq:str) -> pd.DataFrame: 
     arquivo = pd.read_csv(arq)
+    # Padronizando em snake_case
+    column_mapping = {col: col.strip().lower().replace(" ", "_") for col in arquivo.columns}
+    arquivo.rename(columns=column_mapping, inplace=True)
+    # Padronizando valores em strings em para upper case
+    arquivo = arquivo.applymap(lambda x: x.upper() if isinstance(x, str) else x)
+    # Remove acentuacoes
+    arquivo = arquivo.applymap(lambda x: unidecode(x) if isinstance(x, str) else x)
     return arquivo
 
 def define_tipo(x):
     if 'EM' in x.upper() or 'E.M' in x.upper():
         return 'EM'
-    elif 'CIEP' in x.upper() or 'CENTRO INTEGRADO DE EDUCAÇÃO PÚBLICA' in x.upper():
+    elif 'CIEP' in x.upper() or 'CENTRO INTEGRADO DE EDUCAÇAO PUBLICA' in x.upper():
         return 'CIEP'
     elif 'ESCOLA' in x.upper():
         return 'ESCOLA'
     else:
         None
 
-escolas = import_csv(DIR_ESCOLAS)
-print(type(escolas))
+def exec():
+    # import
+    escolas = import_csv(DIR_ESCOLAS)
+    material_didatico = import_csv(DIR_MATERIAL_DIDATICO)
+    sub_prefeituras = import_csv(DIR_SUB_PREFEITURAS)
+    # Merge
+    escolas_quantidade_md = pd.merge(escolas, material_didatico, how='left', on='id')
+    escolas_quantidade_md['tipo'] = escolas_quantidade_md['escolas_postos'].apply(lambda x: define_tipo(x))
+    return escolas_quantidade_md
+
+
+if __name__ == '__main__':
+    df = exec()
+    print(df.head())
