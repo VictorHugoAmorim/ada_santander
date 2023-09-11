ac89d56f4fe33a48a995b329e682992341d53c71
Arthur Sorrentino Ferreira <arthursorrentinoferreira@hotmail.com>
2023-09-11 00:34:26 -0300

    Tratamento do Material Didático (ID e Quantidade)

--- Modulo 3 - Tecnicas de Programacao I (PY)/Projeto Limpeza de dados/src/projeto.py
+++ Modulo 3 - Tecnicas de Programacao I (PY)/Projeto Limpeza de dados/src/projeto.py
@@ -46,13 +46,26 @@ def exec():
         escolas = import_csv(DIR_ESCOLAS_2)
         material_didatico = import_csv(DIR_MATERIAL_DIDATICO_2)
         sub_prefeituras = import_csv(DIR_SUB_PREFEITURAS_2)
-        
+    
+    # Convertendo a coluna Quantidade para o tipo númérico. Os que não puderem ser convertidos, serão NaN.
+    material_didatico['quantidade'] = pd.to_numeric(material_didatico['quantidade'], errors='coerce') 
+    # Alternativa 1: Substituir os NaN por 0
+    # material_didatico['quantidade'].fillna(0, inplace=True) 
+    # Alternativa 2: Excluir as colunas com NaN
+    material_didatico.dropna(subset=['quantidade'], inplace=True)
+    # Convertendo a coluna Quantidade para int
+    material_didatico['quantidade'] = material_didatico['quantidade'].astype(int)
+    # Para visualizar o resultado, descomentar abaixo:
+    material_didatico.to_csv('mat_didatico.csv', index=False)
+
     # Merge
     escolas_quantidade_md = pd.merge(escolas, material_didatico, how='left', on='id')
     escolas_quantidade_md['tipo'] = escolas_quantidade_md['escolas_postos'].apply(lambda x: define_tipo(x))
+    escolas_quantidade_md['id'] = escolas_quantidade_md['id'].apply(lambda x: f'{x:03d}') #Coluna id sempre com 3 dígitos
     return escolas_quantidade_md
 
 
 if __name__ == '__main__':
     df = exec()
     print(df.head())
+    df.to_csv('resultado.csv', index=False)
